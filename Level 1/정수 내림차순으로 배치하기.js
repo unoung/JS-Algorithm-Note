c10738b7abef0a106d59fcfafb81d1f501c587e2
unoung <unoung@icloud.com>
2023-06-02 23:53:40 +0900

    ✍️0602 today solve

---
+++ Level 1/정수 내림차순으로 배치하기.js
@@ -0,0 +1,21 @@
+/* 
+- 문제 설명
+함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+- 제한 조건
+n은 1이상 8000000000 이하인 자연수입니다.
+
+- 입출력 예
+n	    return
+118372	873211 
+*/
+
+//내 풀이
+function solution(n) {
+    let answer = 0;
+    // n을 string으로 변환한후 split로 쪼개서 배열로 만든다.
+    // sort 시킨 후 reverse 후 다시 join으로 합친다
+    answer = n.toString().split("").sort().reverse().join("");
+
+    return Number(answer);
+}
